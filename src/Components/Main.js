105eaa674365907fa2c8b23269d44c47001a1406
Asif Ferdous <asifferdous23@gmail.com>
2023-02-14 19:59:47 +0600

    Rough_Footer

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from './Common/Footer';
 import Navbar from './Common/Navbar';
 
 const Main = () => {
     return (
-        <div>
+        <div className="flex flex-col min-h-screen">
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "./Home";
 import Main from "./Main";
 
 const router=createBrowserRouter([
     {
         path:"/",
         element:<Main></Main>,
+        children:[{
+            path:"/",
+            element:<Home></Home>
+        }]
     }
 ]);
 
